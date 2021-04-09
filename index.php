7ef943d932ea7353bca1320ea1b3bb56cc614d90
picpoint <picpoint@yandex.ru>
2021-04-09 13:17:05 +0300

    work with 8 lesson

--- index.php
+++ index.php
@@ -9,6 +9,35 @@ require __DIR__ . '/autoload.php';
 
 
 
+class Car {
+
+    private $color;
+    private $typeFuel;
+    private $manufacture;
+    private $engine;
+
+
+    public function __construct($color, $typeFuel, $manufacture, $newEngine)
+    {
+        $this->color = $color;
+        $this->typeFuel = $typeFuel;
+        $this->manufacture = $manufacture;
+        $this->engine = $newEngine;
+    }
+
+
+    public function actionStartEngine() {
+        $this->engine->on();
+    }
+
+
+    public function actionStopEngine() {
+        $this->engine->off();
+    }
+
+
+
+}
 
 
 
@@ -16,129 +45,22 @@ require __DIR__ . '/autoload.php';
 
 
 
+class Engine {
+
+    public function on() {
+        echo "Engine on";
+    }
+
+    public function off() {
+        echo "Engine off";
+    }
 
 
+}
 
 
+$engine = new Engine();
 
-
-//$printer = new \Controllers\Printer("HP", "BLACK", "15 page/min");
-//$printer->showParams();
-//echo "<br>";
-//echo "<br>";
-//$printer->actionChangeSpeedPrinting(20);
-//$printer->showParams();
-
-
-
-/** @var
- * Lesson 3
- * 8 object
- * $table */
-//$table = new \Controllers\Table();
-//$table -> size = '1.2 x 1.5 x 0.8';
-//$table -> color = "dark venge";
-//$table -> material = 'tree and iron';
-//$table -> actionShowParams();
-
-
-
-
-
-
-/** @var
- * Lesson 3
- * 7 object
- * $batery */
-//$batery = new \Controllers\Batery();
-//$batery -> size = 'AAA';
-//$batery -> voltage = 1.2;
-//$batery -> capacity = 2700;
-//$batery -> actionShowParams();
-
-
-
-
-
-/** @var
- * Lesson 3
- *6 object
- * $phone */
-//$phone = new \Controllers\Phone();
-//$phone -> manufacturer = 'LG';
-//$phone -> size = '940x480';
-//$phone -> color = 'cyan';
-//$phone -> actionTakeCall();
-//$phone -> actionEndCall();
-//$phone -> actionShowParams();
-
-
-
-
-
-
-
-/** @var
- * Lesson 3
- * 5 object
- * $calc */
-//$calc = new \Controllers\Calculator();
-//$calc -> summ();
-//$calc -> sub();
-//$calc -> multpl();
-//$calc -> diff();
-
-
-
-
-
-/** @var
- * Lesson 3
- * 4 object
- * $printer */
-//$printer = new \Controllers\Printer();
-//$printer -> speedPrinting = 30;
-//$printer -> on();
-//$printer -> printing();
-//$printer -> off();
-
-
-
-
-
-/** @var
- * Lesson 3
- * 3 object
- * $pencil */
-//$pencil = new \Controllers\Pencil();
-//$pencil -> manufacture = 'Moped';
-//$pencil -> color = 'Blue';
-//$pencil -> long = '20 sm';
-//$pencil -> actionDraw();
-//$pencil -> showParams();
-
-
-
-
-
-/** @var
- * Lesson 3
- * 2 object
- * $laptop */
-//$laptop = new \Controllers\Laptop();
-//$laptop -> starting();
-//$laptop -> showVideo();
-//$laptop -> playMusic();
-//$laptop -> ending();
-
-
-
-
-/** @var
- * Lesson 3
- * 1 object
- * $tractor */
-//$tractor = new \Controllers\Tractor();
-//$tractor -> startEngine();
-//$tractor -> toRide();
-//$tractor -> stopEngine();
+$car = new Car('Blue', 'Disel', 'Audi', $engine);
+$car->actionStartEngine();
+$car->actionStopEngine();
