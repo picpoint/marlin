6eacf88067ee37dd213b1e74a53e79ad15b693ff
picpoint <picpoint@yandex.ru>
2021-04-09 14:42:18 +0300

    end work with 8 lesson

--- index.php
+++ index.php
@@ -5,24 +5,29 @@ error_reporting(E_ALL);
 require __DIR__ . '/autoload.php';
 
 
-
+interface EngineInterface {
+    public function on();
+    public function off();
+}
 
 
 
 class Car {
 
-    private $color;
-    private $typeFuel;
-    private $manufacture;
-    private $engine;
+    protected $color;
+    protected $manufacture;
+    protected $typeFuel;
+    protected $dvig;
+    protected $brk;
 
 
-    public function __construct($color, $typeFuel, $manufacture, $newEngine)
+    public function __construct($color, $manufacture, $typeFuel, EngineInterface $dvig, $brk)
     {
         $this->color = $color;
-        $this->typeFuel = $typeFuel;
         $this->manufacture = $manufacture;
-        $this->engine = $newEngine;
+        $this->typeFuel = $typeFuel;
+        $this->engine = $dvig;
+        $this->brakes = $brk;
     }
 
 
@@ -36,33 +41,70 @@ class Car {
     }
 
 
+    public function actionWorkBrakes() {
+        $this->brakes->on();
+    }
 
 }
 
 
 
-
-
-
-
-
-
-class Engine {
+class Engine implements EngineInterface {
 
     public function on() {
         echo "Engine on";
+        echo "<br>";
     }
+
 
     public function off() {
         echo "Engine off";
+        echo "<br>";
     }
-
 
 }
 
 
-$engine = new Engine();
+class OtherEngine implements EngineInterface {
 
-$car = new Car('Blue', 'Disel', 'Audi', $engine);
+    public function on() {
+        echo "Other Engine on";
+        echo "<br>";
+    }
+
+
+    public function off() {
+        echo "Other Engine off";
+        echo "<br>";
+    }
+
+}
+
+
+
+class Brakes {
+
+    public function on() {
+        echo "Brakes is brake";
+        echo "<br>";
+    }
+
+
+    public function off() {
+        echo "Brakes off";
+    }
+
+}
+
+
+
+$dvig = new Engine();
+$dvig2 = new OtherEngine();
+$brk = new Brakes();
+
+$car = new Car('Cyan', 'Hummer', 'Disel', $dvig, $brk);
+
+
 $car->actionStartEngine();
 $car->actionStopEngine();
+$car->actionWorkBrakes();
